d872834b011aa49d84cb06bb503186ecf78d58d2
unknown <tws0321@gmail.com>
2015-10-17 15:20:16 -0700

    added clicking with jquery

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,16 @@
-// Your Javascript will go here!
+// Your Javascript will go here
+function replaceName() {
+	var myNewName = window.prompt('Pick a new name!');
+    document.getElementById('user-name').innerHTML = myNewName;
+}
+
+// document.onclick = replaceName;
+
+$(document).ready(function() {
+  /* Add code from the next steps here */
+
+	$("#copyright").click(function(){ 
+  		$(this).css('color','purple'); 
+});
+
+});
